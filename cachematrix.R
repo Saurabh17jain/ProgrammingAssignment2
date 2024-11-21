18393449468a28218729b9f504be56685bb1734d
Saurabh kumar jain <164175255+Saurabh17jain@users.noreply.github.com>
2024-11-21 23:32:49 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -24,4 +24,15 @@ makeVector <- function(x = numeric()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
 }
